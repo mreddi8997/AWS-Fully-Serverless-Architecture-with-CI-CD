c14d97967ce9eae676203d4d352246841d5863df
Mredd <mreddi8997@gmail.com>
2026-08-16 21:45:32 -0230

    Configure S3 backend for Terraform

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-mohit"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+    use_lockfile = true
+  }
+}
