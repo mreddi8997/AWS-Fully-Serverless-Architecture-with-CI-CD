3db63e64c497fb98c3bfaee8b728d11c72acbbe4
Mredd <mreddi8997@gmail.com>
2026-08-16 21:39:50 -0230

    Add S3 bucket with versioning and encryption

--- Terraform/S3.tf
+++ Terraform/S3.tf
@@ -1,1 +1,35 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-unique-bucket-mohit-123456"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_configuration" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
